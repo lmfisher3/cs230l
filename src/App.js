652bbcf5dfcc23bf2b58e4ced5fb3b094f25ce2b
your user name <your email>
2024-03-03 23:26:33 -0500

    fart

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
       <h2>Section- 002</h2>
       <p>WVU ID: 800400854</p>
       <p>Hi I am Lily</p>
+      <Navbar></Navbar>
+      <card title="CARD ONE" content="I'm not particularly sure what to put in this card"></card>
+      <card title="CARD TWO" content="I'm not particularly sure what to put in this card either"></card>
+      <card title="CARD THREE" content="Or this one. I hope this gets pushed properly."></card>
     </div>
   );
 }
