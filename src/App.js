910437737a579c39989468eff00e89a502a7ae0f
your user name <your email>
2024-03-06 16:29:09 -0500

    hopefully final card fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import logo from './logo.svg';
 import Navbar from './Navbar.js';
-import card from './Card.js';
+import Card from './Card.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1>CS 230L</h1>
       <h2>Section- 002</h2>
       <p>WVU ID: 800400854</p>
       <p>Hi I am Lily</p>
-      <Navbar></Navbar>
-      <card title="CARD ONE" content="I'm not particularly sure what to put in this card"></card>
-      <card title="CARD TWO" content="I'm not particularly sure what to put in this card either"></card>
-      <card title="CARD THREE" content="Or this one. I hope this gets pushed properly."></card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
     </div>
   );
 }
